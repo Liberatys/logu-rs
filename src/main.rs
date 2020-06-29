9f6b35ec5f2881cac58c7c1ffc865eeb43304664
Nick Anthony <Ilithium@protonmail.com>
2020-06-29 17:49:55 +0200

    Adding a first working version that is able to parse json and act on the json to generate the logical table

--- src/main.rs
+++ src/main.rs
@@ -40,30 +40,73 @@ fn run_table_generation(input: Option<Vec<&String>>, output: Option<Vec<&String>
     match input {
         Some(v) => {
             let input_file_content = reader::read_json_file(v[0].as_ref());
-            println!("{}", input_file_content);
+            let parsed = json::parse(input_file_content.as_ref()).unwrap();
             let mut table = table::Table::new();
-            let mut input = HashMap::new();
-            input.insert(0, 1);
-            input.insert(1, 1);
-            let mut output = HashMap::new();
-            output.insert(0, 1);
-            let expression = expression::Expression::new(input, output);
-            table.evaluator.add_expression(expression);
-            table.evaluator.add_input(input::Entry::new(
-                String::from("Klick-Schalter"),
-                0,
-                input::EntryType::INPUT,
-            ));
-            table.evaluator.add_input(input::Entry::new(
-                String::from("Button"),
-                1,
-                input::EntryType::INPUT,
-            ));
-            table.evaluator.add_output(input::Entry::new(
-                String::from("LICHT"),
-                0,
-                input::EntryType::OUTPUT,
-            ));
+            let mut inputs = &parsed["inputs"];
+            let mut outputs = &parsed["outputs"];
+            let mut expressions = &parsed["expressions"];
+            match inputs {
+                json::JsonValue::Array(v) => {
+                    for x in 0..v.len() {
+                        table.evaluator.add_input(input::Entry::new(
+                            String::from(v[x].as_str().unwrap()),
+                            x as i32,
+                            input::EntryType::INPUT,
+                        ));
+                    }
+                }
+                _ => {}
+            }
+            match outputs {
+                json::JsonValue::Array(v) => {
+                    for x in 0..v.len() {
+                        table.evaluator.add_output(input::Entry::new(
+                            String::from(v[x].as_str().unwrap()),
+                            x as i32,
+                            input::EntryType::OUTPUT,
+                        ));
+                    }
+                }
+                _ => {}
+            }
+            match expressions {
+                json::JsonValue::Array(v) => {
+                    for x in 0..v.len() {
+                        let mut input_hash = HashMap::<i32, i32>::new();
+                        let mut output_hash = HashMap::<i32, i32>::new();
+                        let inner_inputs = &v[x]["inputs"];
+                        let inner_outputs = &v[x]["outputs"];
+                        match inner_inputs {
+                            json::JsonValue::Array(v) => {
+                                for x in 0..v.len() {
+                                    let values: Vec<&str> =
+                                        v[x].as_str().unwrap().split("=").collect();
+                                    let first = values[0].parse::<i32>().unwrap();
+                                    let second = values[1].parse::<i32>().unwrap();
+                                    input_hash.insert(first, second);
+                                }
+                            }
+                            _ => {}
+                        }
+                        match inner_outputs {
+                            json::JsonValue::Array(v) => {
+                                for x in 0..v.len() {
+                                    let values: Vec<&str> =
+                                        v[x].as_str().unwrap().split("=").collect();
+                                    let first = values[0].parse::<i32>().unwrap();
+                                    let second = values[1].parse::<i32>().unwrap();
+                                    output_hash.insert(first, second);
+                                }
+                            }
+                            _ => {}
+                        }
+                        table
+                            .evaluator
+                            .add_expression(expression::Expression::new(input_hash, output_hash));
+                    }
+                }
+                _ => {}
+            }
             let headers: Vec<String> = table.evaluator.get_headers();
             let result_table = table.evaluator.evaluate_matrix();
             table.print_table(result_table, headers);
@@ -73,3 +116,4 @@ fn run_table_generation(input: Option<Vec<&String>>, output: Option<Vec<&String>
         }
     }
 }
+
